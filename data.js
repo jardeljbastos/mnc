899fc3d40a9c1019e0d49b8193049d758d0fa540
realidademax <84300151+realidademax@users.noreply.github.com>
2021-06-15 23:56:17 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corredor-salatecnica-medicinanuclear",
+      "name": "Corredor SalaTecnica MedicinaNuclear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.0218789886384663,
+        "pitch": 0.13080034113021455,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8537166812366133,
+          "pitch": 0.3457989451439438,
+          "rotation": 3.141592653589793,
+          "target": "2-salacomando-medicinanuclear"
+        },
+        {
+          "yaw": 2.6443482079348666,
+          "pitch": 0.21942261602294444,
+          "rotation": 5.497787143782138,
+          "target": "3-salalaudos-medicinanuclear"
+        },
+        {
+          "yaw": 1.285447203430138,
+          "pitch": 0.3229105390719518,
+          "rotation": 5.497787143782138,
+          "target": "4-salatecnica-medicinanuclear"
+        },
+        {
+          "yaw": -3.0695405301283785,
+          "pitch": 0.16816593928491663,
+          "rotation": 3.141592653589793,
+          "target": "1-entrada-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada-medicinanuclear",
+      "name": "Entrada MedicinaNuclear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.18116237381912015,
+        "pitch": 0.0178364101541284,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13114984249452455,
+          "pitch": 0.1188818099103095,
+          "rotation": 3.141592653589793,
+          "target": "2-salacomando-medicinanuclear"
+        },
+        {
+          "yaw": -0.23156535660130118,
+          "pitch": -0.01638088762452128,
+          "rotation": 3.141592653589793,
+          "target": "0-corredor-salatecnica-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salacomando-medicinanuclear",
+      "name": "SalaComando MedicinaNuclear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.9690354750745112,
+        "pitch": 0.2760189508888473,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1654400763554946,
+          "pitch": 0.6862766257894055,
+          "rotation": 3.141592653589793,
+          "target": "3-salalaudos-medicinanuclear"
+        },
+        {
+          "yaw": -3.098589165272827,
+          "pitch": 0.28234173241098404,
+          "rotation": 3.141592653589793,
+          "target": "0-corredor-salatecnica-medicinanuclear"
+        },
+        {
+          "yaw": 0.562888793063788,
+          "pitch": 0.33327437907002455,
+          "rotation": 0,
+          "target": "1-entrada-medicinanuclear"
+        },
+        {
+          "yaw": -2.383449114618058,
+          "pitch": 0.07124268290025526,
+          "rotation": 3.141592653589793,
+          "target": "4-salatecnica-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salalaudos-medicinanuclear",
+      "name": "SalaLaudos MedicinaNuclear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.3643331440544877,
+        "pitch": 0.3180826477484544,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13571339867856835,
+          "pitch": 0.75352519668483,
+          "rotation": 3.141592653589793,
+          "target": "2-salacomando-medicinanuclear"
+        },
+        {
+          "yaw": 0.5871155317147085,
+          "pitch": 0.22491548828489094,
+          "rotation": 3.141592653589793,
+          "target": "0-corredor-salatecnica-medicinanuclear"
+        },
+        {
+          "yaw": -0.783029969065284,
+          "pitch": 0.4269599737002139,
+          "rotation": 10.995574287564278,
+          "target": "1-entrada-medicinanuclear"
+        },
+        {
+          "yaw": 1.0716234371232858,
+          "pitch": 0.224501630793565,
+          "rotation": 7.0685834705770345,
+          "target": "4-salatecnica-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salatecnica-medicinanuclear",
+      "name": "SalaTecnica MedicinaNuclear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.2616336302686602,
+        "pitch": -0.10701846092470646,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6012144250060185,
+          "pitch": 0.45736287832114186,
+          "rotation": 3.141592653589793,
+          "target": "5-salatecnica-medicinanuclearii"
+        },
+        {
+          "yaw": 0.27343519180370635,
+          "pitch": 0.10610098631432052,
+          "rotation": 10.995574287564278,
+          "target": "0-corredor-salatecnica-medicinanuclear"
+        },
+        {
+          "yaw": -1.318350813052536,
+          "pitch": 0.0379086973383842,
+          "rotation": 3.141592653589793,
+          "target": "2-salacomando-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salatecnica-medicinanuclearii",
+      "name": "SalaTecnica MedicinaNuclearII",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.5623405935233308,
+        "pitch": 0.3052691992077037,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8752970874980512,
+          "pitch": 0.5376567666170704,
+          "rotation": 3.141592653589793,
+          "target": "4-salatecnica-medicinanuclear"
+        },
+        {
+          "yaw": 0.16727167919793295,
+          "pitch": 0.32509897929633347,
+          "rotation": 5.497787143782138,
+          "target": "0-corredor-salatecnica-medicinanuclear"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
